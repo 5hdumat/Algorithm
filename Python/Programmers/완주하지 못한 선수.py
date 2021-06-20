0a1c5375e6a3badb53b4b5d100fc6148b3769a73
5hdumatt <5hdumatt@gmail.com>
2021-06-20 16:36:36 +0900

    완주하지 못한 선수

--- Python/Programmers/완주하지 못한 선수.py
+++ Python/Programmers/완주하지 못한 선수.py
@@ -59,3 +59,28 @@ def solution(participant, completion):
     return answer
 
 solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])
+
+# 배열을 이용한 문제풀이
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
+
+solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])
+
+# zip 함수를 이용한 문제풀이
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+        
+    return participant[-1]
+
+solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])
