65c3ace6f2d606dc1aead60e5b3e4be634b9ac56
5hdumatt <5hdumatt@gmail.com>
2021-06-29 01:25:37 +0900

    소수 만들기

--- Python/Programmers/소수 만들기.py
+++ Python/Programmers/소수 만들기.py
@@ -1,39 +1,38 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12977
+
 import math
 
-sequence = []
+output = []
 cnt = 0
 
-def dfs(depth, idx, nums, visited):
+def dfs(depth, idx, visited, nums):
     if depth == 3:
-        if is_prime_number(sum(sequence)):
-            global cnt 
+        if is_prime_number(sum(output)):
+            global cnt
             cnt += 1
         return
     
     for i in range(idx, len(nums)):
         if not visited[i]:
             visited[i] = True
-            sequence.append(nums[i])
+            output.append(nums[i])
             
-            dfs(depth + 1, i, nums, visited)
+            dfs(depth + 1, i, visited, nums)
+            
             visited[i] = False
-            
-            sequence.pop()    
-    
+            output.pop()
+
 def is_prime_number(n):
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
-    return True
-            
+    return True 
+    
 def solution(nums):
     visited = [False for _ in range(len(nums))]
     
-    prime_number = is_prime_number(sum(nums))
-    
-    dfs(0, 0, nums, visited)
+    dfs(0, 0, visited, nums)
     
     return cnt
-               
+    
 solution([1,2,7,6,4])
