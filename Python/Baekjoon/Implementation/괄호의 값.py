7dae750b1b1aa9693a4c6e67c6f8bc857078ea36
5hdumat <78165671+5hdumat@users.noreply.github.com>
2021-06-30 09:44:08 +0900

    Update 괄호의 값.py

--- Python/Baekjoon/Implementation/괄호의 값.py
+++ Python/Baekjoon/Implementation/괄호의 값.py
@@ -1,49 +1,129 @@
 # https://www.acmicpc.net/problem/2504
 
-def merge(left, right):
-    i = 0
-    j = 0
-    sorted_list = []
-    
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            sorted_list.append(left[i])
-            i += 1
-        else:
-            sorted_list.append(right[j])
-            j += 1
+# 비효율적인 재귀 연산으로 인한 시간초과 발생
+# https://www.acmicpc.net/problem/2504
+
+# 비효율적인 재귀 연산으로 인한 시간초과 발생
+import sys
+
+# (()[[]])([]) 입력 가정
+brackets = list(sys.stdin.readline().strip())
+
+if len(brackets) % 2 != 0:
+    print(0)
+    exit()
+
+stack = []
+for bracket in brackets:
+    if bracket in ['(', '[']: # ((
+        stack.append(bracket)
+        
+    elif bracket == ')': 
+        cnt = 0
+        
+        while len(stack) != 0:
+            item = stack.pop() # stack => ['(', '('] -> '(', ['(', 2] -> 2
             
-    while i < len(left):
-        sorted_list.append(left[i])
-        i += 1
-    
-    while j < len(right):
-        sorted_list.append(right[j])
-        j += 1
+            if item == '(':
+                if cnt == 0:
+                    stack.append(2) # stack => ['(', 2]
+                else:
+                    stack.append(2 * cnt)
+                break
+            
+            elif item == '[':
+                print(0)
+                exit()
         
-    return sorted_list
-    
-def merge_sort(unsorted_list):
-    if len(unsorted_list) <= 1:
-        return unsorted_list
-    
-    mid = len(unsorted_list) // 2
-    left = unsorted_list[:mid]
-    right = unsorted_list[mid:]
-    
-    left1 = merge_sort(left)
-    right1 = merge_sort(right)
-    
-    return merge(left1, right1)
+            else:
+                cnt += int(item) # 3
+            
+    elif bracket == ']': 
+        cnt = 0
         
-n, m = map(int, input().split())
-
-a = [int(x) for x in input().split()]
-b = [int(x) for x in input().split()]
-
-unsorted_list = a + b
-
-print(merge_sort(unsorted_list))
+        while len(stack) != 0:
+            item = stack.pop() # stack => ['(', '('] -> '(', ['(', 2] -> 2
+            
+            if item == '[':
+                if cnt == 0:
+                    stack.append(3) # stack => ['(', 2]
+                else:
+                    stack.append(3 * cnt)
+                break
+            
+            elif item == '(':
+                print(0)
+                exit()
+        
+            else:
+                cnt += int(item) # 3
+            
+for i in stack:
+    if i in ['(', '[']:
+        print(0)
+    else:
+        print(sum(stack))
     
+    exit()
+
+import sys
+
+# (()[[]])([]) 입력 가정
+brackets = list(sys.stdin.readline().strip())
+
+stack = []
+for bracket in brackets:
+    if bracket in ['(', '[']: # ((
+        stack.append(bracket)
+        
+    elif bracket == ')': 
+        cnt = 0
+        
+        while len(stack) != 0:
+            item = stack.pop() # stack => ['(', '('] -> '(', ['(', 2] -> 2
+            
+            if item == '(':
+                if cnt == 0:
+                    stack.append(2) # stack => ['(', 2]
+                else:
+                    stack.append(2 * cnt)
+                break
+            
+            elif item == '[':
+                print(0)
+                exit()
+        
+            else:
+                cnt += int(item) # 3
+            
+    elif bracket == ']': 
+        cnt = 0
+        
+        while len(stack) != 0:
+            item = stack.pop() # stack => ['(', '('] -> '(', ['(', 2] -> 2
+            
+            if item == '[':
+                if cnt == 0:
+                    stack.append(3) # stack => ['(', 2]
+                else:
+                    stack.append(3 * cnt)
+                break
+            
+            elif item == '(':
+                print(0)
+                exit()
+        
+            else:
+                cnt += int(item) # 3
+
+output = 0        
+for i in stack:
+    if i in ['(', '[']:
+        print(0)
+        exit()
+    else:
+        output += i
+
+print(output)
     
     
