d6e24923b5d26367b02b8a69df81a7f4a457ce08
5hdumatt <5hdumatt@gmail.com>
2021-07-02 00:45:37 +0900

    소수 구하기  문제풀이

--- Python/Baekjoon/Implementation/소수 구하기.py
+++ Python/Baekjoon/Implementation/소수 구하기.py
@@ -1,9 +1,19 @@
 # https://www.acmicpc.net/problem/1929
 m, n = map(int, input().split())
 
-index = 0
-prime_check = [True] * (n + 1)
+prime_check = [True] * (n+1)
+prime_number = []
 
-for i in range(m, n + 1):
-    for j in range(i*2, n + 1, i):
-        print(j)
+for i in range(2, n+1):
+    if prime_check[i]:
+        prime_number.append(i)
+        for j in range(i*2, n+1, i):
+            prime_check[j] = False
+ 
+# 시간: 512 ms     
+print(*list(filter(lambda x : x >= m, prime_number)), sep='\n')
+
+# 시간: 464 ms
+for i in prime_number:
+    if i >= m:
+        print(i)
--- Python/Baekjoon/Implementation/에라토스테네스의 체.py
+++ Python/Baekjoon/Implementation/에라토스테네스의 체.py
@@ -1,15 +1,16 @@
 # https://www.acmicpc.net/problem/2960
+
 n, k = map(int, input().split())
 
+prime_check = [True] * (n+1)
 index = 0
-prime_check = [True] * (n + 1)
+tmp = []
 
-for i in range(2, n + 1):
-    for j in range(i, n + 1, i):
-        if prime_check[j]:
-            prime_check[j] = False
-            index += 1
-            
-            if index == k:
-                print(j)
-    
+for i in range(2, n+1):
+    if prime_check[i]:
+        for j in range(i, n+1, i):
+            if prime_check[j]:
+                prime_check[j] = False
+                tmp.append(j)
+                
+print(tmp[k-1])
