2cfe3478ed61ab0c3547e18a119e8439bd51511e
5hdumatt <5hdumatt@gmail.com>
2021-07-05 00:58:54 +0900

    two-sum 문제풀이

--- Python/Leetcode/longest-palindromic-substring.py
+++ Python/Leetcode/longest-palindromic-substring.py
@@ -1,24 +1,22 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
-import math
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def expand(left: int, right: int) -> str:
-            print(left, right)
-            print('---------')
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-                
-                print(left, right)
-                print(s[left + 1:right])
-                print('===========')
-                
-            return s[left + 1:right]
             
+            return s[left+1:right]
+        
+        if len(s) < 2:
+            return s
+        
         result = ''
-        for i in range(len(s)):
+        for i in range(len(s)-1):
             result = max(result, expand(i, i+1), expand(i, i+2), key=len)
-            
+        
+        return result
+    
 a = Solution()
-a.longestPalindrome("123454321")
+a.longestPalindrome('a')
