c2f106caf2ce02b8673808eaf77a78f1bfe563fc
5hdumatt <5hdumatt@gmail.com>
2021-10-01 01:58:21 +0900

    정다면체 문제풀이

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/코드 구현력 기르기/대표값.py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/코드 구현력 기르기/대표값.py
@@ -19,3 +19,43 @@ for index, value in enumerate(score_list):
 
 print(avg, result)
 
+'''
+위 코드는 오류가 있다.
+
+파이썬에서의 round() 함수는 round_half_even 방식이다.
+round_half_even의 경우 정확하게 4.50000과 같이 값이 정확히 하프 지점에 있다면 짝수 지점에 근사값을 지정한다.
+
+예)
+a = 4.500
+print(rount(a)) # 4
+
+a = 4.5111
+print(rount(a)) # 5
+
+round()가 아닌
+
+a = 66.5
+a = a+0.5
+a = int(a) 와 같은 방식으로 사용해야 한다.
+'''
+
+# 오류 수정
+n = int(input())
+score_list = list(map(int, input().split()))
+avg = int((sum(score_list) / n) + 0.5)
+min = 2147483648
+
+for index, value in enumerate(score_list):
+    # 거리값 구하기
+    tmp = abs(value - avg)
+
+    if tmp < min:
+        min = tmp
+        score = value
+        result = index + 1
+
+    elif tmp == min and score < value:
+        score = value
+        result = index + 1
+
+print(avg, result)
---
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/코드 구현력 기르기/정다면체.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+cnt = [0] * (n + m + 1)
+max = -214700000 # 0이라고 해도 됨
+result = []
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        cnt[i+j] += 1
+
+for i in range(n + m + 1):
+    if cnt[i] > max:
+        max = cnt[i]
+
+for i in range(n + m + 1):
+    if cnt[i] == max:
+        print(i, end=' ')
