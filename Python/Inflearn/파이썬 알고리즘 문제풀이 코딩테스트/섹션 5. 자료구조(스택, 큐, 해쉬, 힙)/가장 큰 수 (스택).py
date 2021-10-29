7a7fddc55c04dcfc95b2ae3517f1ec14b67c9a8e
5hdumatt <5hdumatt@gmail.com>
2021-10-29 22:30:03 +0900

    가장 큰 수, 쇠막대기 스택 문제풀이

---
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/섹션 5. 자료구조(스택, 큐, 해쉬, 힙)/가장 큰 수 (스택).py
@@ -0,0 +1,48 @@
+from collections import deque
+
+n, m = map(int, input().split())
+n = deque(list(map(int, str(n))))
+
+stack = []
+cnt = 0
+while len(n) > 0:
+    tmp = n.popleft()
+
+    if len(stack) == 0:
+        stack.append(tmp)
+        continue
+
+    for _ in range(len(stack)):
+        if stack[-1] < tmp and m > 0:
+            stack.pop()
+            m -= 1
+        elif stack[-1] > tmp:
+            stack.append(tmp)
+            break
+    else:
+        stack.append(tmp)
+
+if m == 0:
+    print(*stack, sep='')
+else:
+    print(*stack[:-m], sep='')  # 슬라이싱을 -2까지 하면 9977252641에서 99772526 출력
+
+# 강의 문제 풀이
+
+num, m = map(int, input().split())
+num = list(map(int, str(num)))
+stack = []
+
+for x in num:
+    # stack이 비어있지 않고 m이 0보다 크고 stack의 가장 마지막 인자가 x보다 작으면 반복문 돌리기
+    while stack and m > 0 and stack[-1] < x:
+        stack.pop()
+        m -= 1
+    stack.append(x)
+
+if m != 0:
+    stack = stack[:-m]
+
+res = ''.join(map(str, stack))
+print(res)
+
---
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/섹션 5. 자료구조(스택, 큐, 해쉬, 힙)/쇠막대기 (스택).py
@@ -0,0 +1,18 @@
+s = input()
+stack = []
+sum = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append((s[i]))
+    else:
+        stack.pop()
+
+        if s[i-1] == '(': # '('가 아닌 else문 이니 and s[i] == ')' 생략
+            # stack.pop()
+            sum += len(stack)
+        elif s[i-1] == ')':
+            # stack.pop()
+            sum += 1
+
+print(sum)
