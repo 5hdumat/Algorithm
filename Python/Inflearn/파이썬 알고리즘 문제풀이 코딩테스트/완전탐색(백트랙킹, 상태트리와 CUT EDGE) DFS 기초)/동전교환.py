40510bf1b82e37dd28dbbb0bfb0eef4fff1c3339
5hdumatt <5hdumatt@gmail.com>
2021-11-08 00:11:13 +0900

    동전교환 문제풀이

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/동전교환.py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/동전교환.py
@@ -2,6 +2,10 @@
 3
 1 2 5
 15
+
+4
+1 2 5 7
+14
 '''
 
 import sys
@@ -10,9 +14,43 @@ import sys
 def dfs(l, sum):
     global res
 
-    if sum > m:
-        print(res)
+    if l > res:
         return
+
+    if sum > m:
+        return
+
+    if sum == m:
+        if l <= res:
+            res = l
+    else:
+        for i in range(n):
+            dfs(l + 1, sum + c[i])
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    c = list(map(int, sys.stdin.readline().split()))
+    m = int(sys.stdin.readline())
+    # c.sort(reverse=True)
+    res = 9999999999999
+
+    dfs(0, 0)
+    print(res)
+
+# 강의 문제 풀이 (큰 동전부터 카운트)
+import sys
+
+
+def dfs(l, sum):
+    global res
+    if sum > m or l > res:
+        return
+
+    if sum == m:
+        print(l)
+        sys.exit(0)
+
     else:
         for i in range(n):
             dfs(l + 1, sum + c[i])
@@ -23,5 +61,8 @@ if __name__ == "__main__":
     c = list(map(int, sys.stdin.readline().split()))
     m = int(sys.stdin.readline())
 
-    res = 0
+    res = 9999999999999
+
+    c.sort(reverse=True)
     dfs(0, 0)
+    print(res)
