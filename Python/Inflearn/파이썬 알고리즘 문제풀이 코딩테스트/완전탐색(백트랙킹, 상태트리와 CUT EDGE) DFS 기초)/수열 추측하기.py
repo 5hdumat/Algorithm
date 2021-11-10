e540e007a39023c16d354b3c5d2bbc7c1c17f62c
5hdumatt <5hdumatt@gmail.com>
2021-11-11 01:24:57 +0900

    수열 추측하기 문제풀이

---
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/수열 추측하기.py
@@ -0,0 +1,62 @@
+import sys
+
+
+def dfs(l):
+    if l == n:
+        sum = 0
+
+        for i, v in enumerate(p):
+            sum += b[i] * v
+
+        if sum == f:
+            print(*p)
+            sys.exit(0)
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[l] = i
+                dfs(l + 1)
+                ch[i] = 0
+
+
+n, f = map(int, input().split())
+p = [0] * n
+b = [1] * n
+ch = [0] * (n + 1)
+
+for i in range(1, n):
+    b[i] = (b[i - 1] * (n - i)) / i
+
+b = list(map(int, b))
+
+dfs(0)
+
+# 강의 문제 풀이
+import sys
+
+
+def dfs(l, sum):
+    if l == n and sum == f:
+        print(*p)
+        sys.exit(0)
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[l] = i
+                dfs(l + 1, sum + (p[l] * b[l]))
+                ch[i] = 0
+
+
+n, f = map(int, input().split())
+p = [0] * n
+b = [1] * n
+ch = [0] * (n + 1)
+
+for i in range(1, n):
+    b[i] = (b[i - 1] * (n - i)) / i
+
+b = list(map(int, b))
+
+dfs(0, 0)
