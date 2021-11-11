4cf03b37f40d95887782c8caec4d7dd25032cb1a
5hdumatt <5hdumatt@gmail.com>
2021-11-12 02:05:11 +0900

    수열 추측하기 문제풀이 복습

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/수열 추측하기.py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/완전탐색(백트랙킹, 상태트리와 CUT EDGE) DFS 기초)/수열 추측하기.py
@@ -1,40 +1,38 @@
 import sys
 
-
-def dfs(l):
-    if l == n:
-        sum = 0
-
-        for i, v in enumerate(p):
-            sum += b[i] * v
-
-        if sum == f:
-            print(*p)
-            sys.exit(0)
-    else:
-        for i in range(1, n + 1):
-            if ch[i] == 0:
-                ch[i] = 1
-                p[l] = i
-                dfs(l + 1)
-                ch[i] = 0
-
-
-n, f = map(int, input().split())
-p = [0] * n
-b = [1] * n
-ch = [0] * (n + 1)
-
-for i in range(1, n):
-    b[i] = (b[i - 1] * (n - i)) / i
-
-b = list(map(int, b))
-
-dfs(0)
+# def dfs(l):
+#     if l == n:
+#         sum = 0
+#
+#         for i, v in enumerate(p):
+#             sum += b[i] * v
+#
+#         if sum == f:
+#             print(*p)
+#             sys.exit(0)
+#     else:
+#         for i in range(1, n + 1):
+#             if ch[i] == 0:
+#                 ch[i] = 1
+#                 p[l] = i
+#                 dfs(l + 1)
+#                 ch[i] = 0
+#
+#
+# n, f = map(int, input().split())
+# p = [0] * n
+# b = [1] * n
+# ch = [0] * (n + 1)
+#
+# for i in range(1, n):
+#     b[i] = (b[i - 1] * (n - i)) / i
+#
+# b = list(map(int, b))
+#
+# dfs(0)
 
 # 강의 문제 풀이
 import sys
-
 
 def dfs(l, sum):
     if l == n and sum == f:
@@ -45,18 +43,15 @@ def dfs(l, sum):
             if ch[i] == 0:
                 ch[i] = 1
                 p[l] = i
-                dfs(l + 1, sum + (p[l] * b[l]))
+                dfs(l + 1, sum + (i * b[l]))
                 ch[i] = 0
-
 
 n, f = map(int, input().split())
 p = [0] * n
 b = [1] * n
 ch = [0] * (n + 1)
 
-for i in range(1, n):
-    b[i] = (b[i - 1] * (n - i)) / i
-
-b = list(map(int, b))
+for i in range(1, n - 1):
+    b[i] = b[i - 1] * (n - i) // i
 
 dfs(0, 0)
