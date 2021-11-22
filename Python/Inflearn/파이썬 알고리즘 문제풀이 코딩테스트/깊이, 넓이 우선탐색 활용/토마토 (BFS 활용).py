333763eba62336a2f1cc2753dae5fc9efe924a90
5hdumatt <5hdumatt@gmail.com>
2021-11-23 01:18:22 +0900

    안전영역(BFS, DFS 활용), 토마토 문제풀이

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/깊이, 넓이 우선탐색 활용/토마토 (BFS 활용).py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/깊이, 넓이 우선탐색 활용/토마토 (BFS 활용).py
@@ -5,6 +5,7 @@
 0 0 -1 0 0 0
 0 0 0 0 -1 1
 '''
+# 최단거리 문제는 BFS 활용하자!
 from collections import deque
 
 dx = [-1, 0, 1, 0]
@@ -14,23 +15,40 @@ n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(m)]
 dis = [[0] * n for _ in range(m)]
 
-day = 0
 Q = deque()
 
+# 일단 시작지점 싹 정리
 for i in range(m):
     for j in range(n):
         if board[i][j] == 1:
-            dis += 1
             Q.append((i, j))
 
-            while Q:
-                tmp = Q.popleft()
+while Q:
+    tmp = Q.popleft()
 
-                for i in range(4):
-                    x = tmp[0] + dx[i]
-                    y = tmp[1] + dy[i]
+    for i in range(4):
+        x = tmp[0] + dx[i]
+        y = tmp[1] + dy[i]
 
-                    if 0 <= x < m and 0 <= y < n and board[x][y] == 0:
-                        board[x][y] = 1
+        if 0 <= x < m and 0 <= y < n and board[x][y] == 0:
+            dis[x][y] = dis[tmp[0]][tmp[1]] + 1
+            board[x][y] = 1
+            Q.append((x, y))
 
-print(day)
+flag = True
+for i in range(m):
+    for j in range(n):
+        if board[i][j] == 0:
+            flag = False
+            break
+
+if flag:
+    res = 0
+    for i in range(m):
+        for j in range(n):
+            if dis[i][j] > res:
+                res = dis[i][j]
+
+    print(res)
+else:
+    print(-1)
