221c57d3df158d183ccfba210cba4ba29b7f5f36
5hdumatt <5hdumatt@gmail.com>
2021-11-27 22:04:29 +0900

    LIS 응용 문제풀이

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/Dynamic programming(동적계획법)/가장 높은 탑 쌓기.py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/Dynamic programming(동적계획법)/가장 높은 탑 쌓기.py
@@ -15,7 +15,7 @@ res = 0
 for i in range(1, n):
     tmp = 0
     for j in range(i - 1, -1, -1):  # 위에서 0부터 시작하므로 -1까지인거 주의
-        if arr[i][2] < arr[j][2] and dy[j] > tmp: # 기준이 되는 벽돌보다 비교군 벽돌이 더 커야한다. (여기서 기준 벽돌은 탑의 최상단에 놓여져있다.)
+        if arr[i][2] < arr[j][2] and dy[j] > tmp:  # 기준이 되는 벽돌보다 비교군 벽돌이 더 커야한다. (여기서 기준 벽돌은 탑의 최상단에 놓여져있다.)
             tmp = dy[j]
 
     dy[i] = tmp + arr[i][1]
