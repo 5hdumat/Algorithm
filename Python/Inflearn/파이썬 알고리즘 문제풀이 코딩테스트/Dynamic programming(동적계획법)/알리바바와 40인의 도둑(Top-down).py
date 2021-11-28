858f097a53ecbd59093026d95ad14db2fc1277e2
5hdumatt <5hdumatt@gmail.com>
2021-11-29 02:06:44 +0900

    알리바바와 40인의 도둑 Top-Down 문제풀이 (이건 다시보자..)

--- Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/Dynamic programming(동적계획법)/알리바바와 40인의 도둑(Top-down).py
+++ Python/Inflearn/파이썬 알고리즘 문제풀이 코딩테스트/Dynamic programming(동적계획법)/알리바바와 40인의 도둑(Top-down).py
@@ -7,23 +7,31 @@
 1 7 5 2 4
 '''
 
+
 # 이건 도착점부터 출발한다.
 def DFS(x, y):
+    if dy[x][y] > 0:
+        return dy[x][y]
+
     if x == 0 and y == 0:
         return arr[x][y]
 
     else:
         if y == 0:
-            return DFS(x - 1, y) + arr[x][y]
+            dy[x][y] = DFS(x - 1, y) + arr[x][y]
 
         elif x == 0:
-            return DFS(x, y - 1) + arr[x][y]
+            dy[x][y] = DFS(x, y - 1) + arr[x][y]
 
         else:
-            return min(DFS(x-1, y), DFS(x, y-1)) + arr[x][y]
+            dy[x][y] = min(DFS(x - 1, y), DFS(x, y - 1)) + arr[x][y]
+
+        return dy[x][y]
 
 
 if __name__ == "__main__":
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
+    dy = [[0] * n for _ in range(n)]
+
     print(DFS(n - 1, n - 1))
