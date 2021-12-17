a061af83bead93086da1624b1016d59af14f6515
5hdumatt <5hdumatt@gmail.com>
2021-12-17 22:58:52 +0900

    백준 하노이의 탑 문제풀이

---
+++ Baekjoon/백트래킹/하노이의 탑.py
@@ -0,0 +1,26 @@
+def move(no, x, y):
+    if n == 1:
+        res.append((x, y))
+        return
+
+    if no > 1:
+        move(no - 1, x, 6 - x - y)
+
+    res.append((x, y))
+
+    if no > 1:
+        move(no - 1, 6 - x - y, y)
+
+
+n = int(input())
+
+if n <= 20:
+    res = []
+    move(n, 1, 3)
+
+    print((2 ** n) - 1)
+    for x, y in res:
+        print(f'{x} {y}')
+else:
+    # 이동 횟수 2**n-1
+    print((2 ** n) -1)
