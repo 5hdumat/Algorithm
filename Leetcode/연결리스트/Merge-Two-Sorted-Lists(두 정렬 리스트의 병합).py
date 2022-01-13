1e79aa62495d374cb776dcbf35d80e2975f9a24b
5hdumatt <5hdumatt@gmail.com>
2022-01-13 20:33:30 +0900

    연결리스트 알고리즘 문제풀이

--- Leetcode/연결리스트/Merge-Two-Sorted-Lists(두 정렬 리스트의 병합).py
+++ Leetcode/연결리스트/Merge-Two-Sorted-Lists(두 정렬 리스트의 병합).py
@@ -12,15 +12,115 @@ class ListNode:
         self.val = val
         self.next = next
 
+'''
+pass 0
 
+1 -> 2 -> 4
+1 -> 3 -> 4
+
+pass 1
+1
+
+2 -> 4
+1 -> 3 -> 4
+
+pass 2
+
+1
+
+1 -> 3 -> 4
+2 -> 4
+
+pass 3
+
+1 -> 1
+
+3 -> 4
+2 -> 4
+
+pass 4
+
+1 -> 1
+
+2 -> 4
+3 -> 4
+
+pass 5
+
+1 -> 1 -> 2
+
+4
+3 -> 4
+
+pass 6
+
+1 -> 1 -> 2 
+
+3 -> 4
+4
+
+pass 7
+
+1 -> 1 -> 2 -> 3
+
+4
+4
+
+pass 8
+
+1 -> 1 -> 2 -> 3
+
+None
+4
+
+pass 9
+
+
+1 -> 1 -> 2 -> 3 -> 4 
+
+4
+None
+
+
+pass 10
+
+
+1 -> 1 -> 2 -> 3 -> 4 -> 4
+
+None
+None
+
+
+pass 11
+
+
+1 -> 1 -> 2 -> 3 -> 4 -> 4
+
+None
+None
+
+'''
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+    def dump(self, l1):
+        while l1:
+            print(l1.val, end=' -> ')
+            l1 = l1.next
+        print(None)
 
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if (not l1) or (l2 and l1.val > l2.val):
+            print(not l1)
+            l1, l2 = l2, l1
+
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+
+        return l1
 
 s = Solution()
 
 input1 = ListNode(1, ListNode(2, ListNode(4, None)))
 input2 = ListNode(1, ListNode(3, ListNode(4, None)))
 
-s.mergeTwoLists(input1, input2)
+lists = s.mergeTwoLists(input1, input2)
+s.dump(lists)
---
+++ Leetcode/연결리스트/reverse-linked-list(연결리스트 뒤집기).py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def dump(self, head):
+        while head:
+            print(head.val, end=' -> ')
+            head = head.next
+        print(None)
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+
+        while head:
+            next, head.next = head.next, prev
+            prev, head = head, next
+
+        return prev
+
+    def reverseList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(node, prev=None):
+            if not node:
+                return prev
+
+            next, node.next = node.next, prev
+            return reverse(next, node)
+
+        return reverse(head)
+
+
+s = Solution()
+
+input = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, None)))))
+lists = s.reverseList2(input)
+s.dump(lists)
