e8352e7786b701755ae0330d65628ca65520be8d
5hdumatt <5hdumatt@gmail.com>
2022-01-19 22:33:57 +0900

    스택 알고리즈 문제풀이 중복 문자 제거

---
+++ Leetcode/스택과 큐/remove-duplicate-letters(중복 문자 제거).py
@@ -0,0 +1,36 @@
+import collections
+
+
+class Solution:
+    def removeDuplicateLettersNonRecur(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for x in s:
+            counter[x] -= 1
+
+            if x in seen:
+                continue
+
+            while stack and x < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
+            stack.append(x)
+            seen.add(x)
+
+        return ''.join(stack)
+
+
+    def removeDuplicateLetters(self, s: str) -> str:
+        # s: cbacdcbc
+        for x in sorted(set(s)):
+            suffix = s[s.index(x):]
+
+            if set(suffix) == set(s):
+                return x + self.removeDuplicateLetters(suffix.replace(x, ''))
+
+        return ''
+
+s = Solution()
+letters = s.removeDuplicateLettersNonRecur("cbacdcbc")
+print(letters)
+
