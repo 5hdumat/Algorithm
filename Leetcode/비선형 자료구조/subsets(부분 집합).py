3acceac646a019ea34dc714f883e4f241660fae6
5hdumatt <5hdumatt@gmail.com>
2022-02-02 21:24:02 +0900

    DFS 문제풀이

---
+++ Leetcode/비선형 자료구조/subsets(부분 집합).py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/subsets/
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def _dfs(l, s):
+            res.append(tmp[:])
+
+            for i in range(s, len(nums)):
+                if ch[i] == 0:
+                    ch[i] = 1
+                    tmp.append(nums[i])
+
+                    _dfs(l + 1, i)
+
+                    ch[i] = 0
+                    tmp.pop()
+
+        tmp = []
+        ch = [0] * (len(nums) + 1)
+        res = []
+        _dfs(0, 0)
+
+        return res
+
+
+s = Solution()
+s.subsets([0])
