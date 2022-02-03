8bf86f5203eda1504452ea87b7a23191c0382a77
5hdumatt <5hdumatt@gmail.com>
2022-02-03 19:58:11 +0900

    DFS 문제풀이

---
+++ Leetcode/비선형 자료구조/cource-schedule(코스 스케줄).py
@@ -0,0 +1,42 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def _dfs(s):
+            if s in traced:
+                return False
+
+            # 이미 방문한 곳은 순환구조임을 검증하지 않아도 된다.
+            if s in visited:
+                return True
+
+            traced.add(s)
+
+            for y in graph[s]:
+                if not _dfs(y):
+                    return False
+
+            traced.remove(s)
+            visited.add(s)
+
+            return True
+
+        graph = collections.defaultdict(list)
+
+        for a, b in prerequisites:
+            graph[a].append(b)
+
+        traced = set()
+        visited = set()
+        for x in list(graph):
+            if not _dfs(x):
+                return False
+
+        return True
+
+
+s = Solution()
+finish = s.canFinish(2, [[1, 0], [2, 0]])
+print(finish)
