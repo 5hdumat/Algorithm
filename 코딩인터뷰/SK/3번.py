f0255b56f52dc88aa194f944d66c21c9616804a4
5hdumatt <5hdumatt@gmail.com>
2022-03-15 00:21:28 +0900

    SK 정사각형 최단거리 경우의수 문제풀이

--- 코딩인터뷰/SK/3번.py
+++ 코딩인터뷰/SK/3번.py
@@ -1,6 +1,25 @@
+import math
+
+
 def solution(width, height, diagonals):
+    tables = [[0] * (height + 1) for _ in range(width + 1)]
+
+    for i, table in enumerate(tables):
+        for j, row in enumerate(table):
+            if i == 0 or j == 0:
+                tables[i][j] = 1
+            else:
+                tables[i][j] = tables[i - 1][j] + tables[i][j - 1]
+
     answer = 0
-    return answer
+
+    for dx, dy in diagonals:
+        answer += tables[dx - 1][dy] * tables[width - dx][height - dy + 1]
+        answer += tables[dx][dy - 1] * tables[width - dx + 1][height - dy]
+
+    print(int(answer % 10000019))
 
 
 solution(2, 2, [[1, 1], [2, 2]])
+solution(3, 2, [[1, 2], [3, 2], [3, 1]])
+solution(51, 37, [[17, 19]])
