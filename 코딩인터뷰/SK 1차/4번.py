7c96760a6cc7644c1187515d337b0f67a2300ccb
5hdumatt <5hdumatt@gmail.com>
2022-03-19 00:28:12 +0900

    알고리즘 문제풀이

--- 코딩인터뷰/SK 1차/4번.py
+++ 코딩인터뷰/SK 1차/4번.py
@@ -4,7 +4,7 @@ import collections
 def solution(n, edges):
     def _dfs(l):
         if len(tmp) == 3:
-            print(tmp)
+            answer.append(tmp[:])
             return
 
         for x in graph[l]:
@@ -13,16 +13,23 @@ def solution(n, edges):
                 _dfs(x)
                 tmp.pop()
 
+        return 0
+
     graph = collections.defaultdict(list)
+
     for i, j in edges:
         graph[i].append(j)
         graph[j].append(i)
 
     tmp = []
+    answer = []
     for i in range(n):
         tmp.append(i)
         _dfs(i)
         tmp.pop()
 
+    return (len(answer) * 2)
+
 
 solution(5, [[0, 1], [0, 2], [1, 3], [1, 4]])
+solution(4, [[2, 3], [0, 1], [1, 2]])
