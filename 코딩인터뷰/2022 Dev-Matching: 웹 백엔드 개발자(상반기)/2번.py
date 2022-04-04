ae3f4d6a0b0b99dbdd379d5087ecb1b7ef221ea8
5hdumatt <5hdumatt@gmail.com>
2022-04-04 21:02:35 +0900

    2022 상반기 데브매칭 못풀었던 2번 문제 풀이

--- 코딩인터뷰/2022 Dev-Matching: 웹 백엔드 개발자(상반기)/2번.py
+++ 코딩인터뷰/2022 Dev-Matching: 웹 백엔드 개발자(상반기)/2번.py
@@ -1,15 +1,60 @@
+from collections import deque
+from copy import deepcopy
 from itertools import product
 
 
 def solution(grid):
-    answer = -1
+    answer = 0
 
-    repeat = 0
-    for x in grid:
-        repeat += x.count('?')
+    repeat = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == '?':
+                repeat.append((i, j))
 
-    # for x in list(product(['a', 'b', 'c'], repeat=repeat)):
-    #     print(x)
+    permutations = list(product(['a', 'b', 'c'], repeat=len(repeat)))
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    for index, permutation in enumerate(permutations):
+        graph = []
+
+        for x in grid:
+            tmp = []
+            for cha in x:
+                tmp.append(cha)
+
+            graph.append(tmp)
+
+        for i, v in enumerate(repeat):
+            graph[v[0]][v[1]] = permutation[i]
+
+        dq = deque()
+        checked = []
+        cnt = 0
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                if graph[i][j] != 0 and graph[i][j] not in checked:
+                    cnt += 1
+                    prev = graph[i][j]
+                    checked.append(graph[i][j])
+                    graph[i][j] = 0
+                    dq.append((i, j))
+
+                    while dq:
+                        tmp = dq.popleft()
+
+                        for k in range(4):
+                            x = tmp[0] + dx[k]
+                            y = tmp[1] + dy[k]
+                            if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and prev == graph[x][y]:
+                                cnt += 1
+                                graph[x][y] = 0
+                                dq.append((x, y))
+
+        if cnt == len(grid) * len(grid[0]):
+            answer += 1
 
     return answer
 
